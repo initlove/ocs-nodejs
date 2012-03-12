7f8b82ab801f9dd10515c630cbeee6b377a51ac8
David Liang <liangchenye@gmail.com>
2012-03-12 18:59:46 +0800

    add vote, but not test

--- index.js
+++ index.js
@@ -5,10 +5,8 @@
 
 var comments = require('./service/comments');
 var express = require('express')
-var mongo = require('mongodb');
 
 var app = module.exports = express.createServer();
-
 // Configuration
 
 app.configure(function(){
@@ -22,32 +20,6 @@ app.configure('development', function(){
 
 app.configure('production', function(){
   app.use(express.errorHandler());
-});
-
-/*
-db = new mongo.Db("test", new mongo.Server('localhost', 27017, {}), {});
-db.open(function() {
-	db.collection("things", function(err, collection) {
-		collection.find(function(err, cursor) {
-			cursor.toArray(function(err, items) {
-				var len = items.length;
-				console.log(len);
-				for (var i = 0; i < len; i++) {
-					console.log("type "+ items[i].type);
-					console.log("parent " + items[i].parent);
-					console.log("content "+ items[i].content);
-					console.log("content2 "+ items[i].content2);
-					console.log("subject "+ items[i].subject);
-					console.log("message "+ items[i].message);
-				}
-			});
-		});
-	});
-});
-*/
-
-app.get('/user/:id', function(req, res){
-	res.send('user ' + req.params.id);
 });
 
 app.get('/comments/get', comments.get);
--- service/comments.js
+++ service/comments.js
@@ -25,7 +25,7 @@ exports.get = function (req, res){
     if (params.pagesize != undefined)
         pagesize = params.pagesize;
 
-	$('test.things').find(pagesize, page * pagesize, {"type": params.type, "content":params.content} ,
+	$('test.comments').find(pagesize, page * pagesize, {"type": params.type, "content":params.content} ,
 		function (r) {
             var data;
             data = JSON.stringify(r.documents);
@@ -76,12 +76,12 @@ function add_comment (req, res) {
        content2 = req.body["content2"];
 
    /* TODO: Check the validation of 'parent'*/
-   $('test.things').find({"content": content},
+   $('test.comments').find({"content": content},
         function(r) {
             if (r.documents.length == 0) {
                 res.send (utils.message (utils.meta (105, "content id invalid")));
             } else {
-	            $('test.things').insert(
+	            $('test.comments').insert(
 			            {"type" :req.body["type"],
 			       		"content" :req.body["content"],
     					"content2":content2,
@@ -93,7 +93,7 @@ function add_comment (req, res) {
      	});
 }
 
-exports.add = function (req, res){
+exports.add = function (req, res) {
     if (!check_type (req.body["type"], res))
         return;
     /* Donnot check the subject */
@@ -105,4 +105,90 @@ exports.add = function (req, res){
     add_comment (req, res);
 
     return;
+};
+
+vote_comment = function (req, res) {
+    var date = Date();
+    //TODO: get personid
+    var personid;
+    /* add to votes table */
+    $('test.votes').insert(
+        {"commentid" :req.body["commentid"],
+        "score" :req.body["score"],
+        "personid" :personid,
+        "date" : date,
+        });
+       			
+    /* update the summary table */
+    $('test.summary').find({"commentid": req.body["commentid"]},
+        function (r) {
+            var score;
+            if (r.documents.length == 1) {
+                score = r.documents[0].score;
+                var count = r.documents[0].count;
+                var total = count * score + req.body["score"];
+                count ++;
+                score = total / count;
+                $('test.summary').update({"commentid": req.body["commentid"]},
+                    {"commentid": req.body["commentid"],
+                    "count":count,
+                    "score":score});
+            } else {
+                score = req.body["score"];
+                $('test.summary').insert({
+                    "commentid": req.body["commentid"],
+                    "count": 1,
+                    "score": score});
+            }
+            /* update the comment table */
+            $('test.comments').find({"commentid" : req.body["commentid"]},
+                function (r) {
+                    $('test.comments').update({"commentid" : req.body["commentid"]},
+                        {"commentid", r.documents[0].commentid,
+                        "type", r.documents[0].type,
+                        "content", r.documents[0].content,
+                        /*  "content2", r.documents[0].content2, */
+                        "parent", r.documents[0].parent,
+                        /*  "subject", r.documents[0].subject,  */
+                        "message", r.documents[0].message,
+                        "score", score});
+                });
+        });
+    res.send (utils.message (utils.meta (100)));
+};
+
+exports.vote = function (req, res) {
+    var score = req.body["score"];
+    var score_valid = false;
+    if (score != undefined) {
+        if ((score >= 0) && (score <= 100)) {
+            score_valid = true;
+        }
+    }
+    if (score_valid == false) {
+        res.send (utils.message (utils.meta (102, "score is invalid")));
+        return;
+    }
+
+    var commentid = req.body["commentid"];
+    if ((commentid == undefined) || (commentid.length == 0)) {
+        res.send (utils.message (utils.meta (101, "comment is empty")));
+        return;
+    }
+
+    $('test.comments').find({"commentid": commentid},
+        function(r) {
+            if (r.documents.length == 0) {
+                res.send (utils.message (utils.meta (101, "comment is empty")));
+            } else {
+                $('test.votes').find({"commentid": commentid, "personid": personid},
+                    function (r) {
+                        if (r.documents.length == 0) {
+                            vote_comment (req, res);
+                        } else {
+                            res.send (utils.message (utils.meta (103, "you have already voted on this comment")));
+                        }
+                    });
+            }
+     	});
 };
