bbaf299f1a86878c75fef9951812aa055ea2e2dd
David Liang <liangchenye@gmail.com>
2012-03-13 16:30:33 +0800

    make the push, list work , use save instand of insert

--- compose/list.js
+++ compose/list.js
@@ -1,4 +1,30 @@
-var $ = require("mongous").Mongous;
+var $ = require('mongous').Mongous;
+var db = require('mongodb').Db;
+var server = require('mongodb').Server;
+
+
+var client = new db('test', new server('127.0.0.1', 27017, {}));
+
+var list_all = function (err, collection) {
+    collection.find().toArray(function(err, results) {
+        console.log (results);
+    });
+};
+
+client.open(function(err, Client) {
+    client.collection('category', list_all);
+    client.collection('content', list_all);
+});
+
+
+return;
+
+/*TODO: donnot know why, the mongous always fail to connect .. */
+$('test.category').find (
+        function (r) {
+            console.log (r.documents);
+        }
+        );
 
 $('test.content').find(
         function (r) {
--- compose/push.js
+++ compose/push.js
@@ -1,6 +1,25 @@
 var fs = require('fs');
 var $ = require("mongous").Mongous;
 
+add_content = function (app, repo) {
+    /*FIXME: json add a attribute? how to ? */
+    var str = JSON.stringify(app);
+    var pos = str.lastIndexOf('}');
+    var new_str = str.substr(0, pos) + ","
+                + "\"repo\":" + "\"" + repo + "\"}";
+    var new_object = JSON.parse (new_str);
+    /*TODO: check, update */
+    $('test.content').save (new_object);
+};
+
+add_category = function (app) {
+    console.log (app.appcategories);
+    for (var i = 0; i < app.appcategories.length; i++) {
+        var category = app.appcategories[i];
+        $('test.category').save ({"name": category});
+    }
+};
+
 fs.readFile('./appdata.json', function (err, data) {
     if (err)
         console.log ("error in load appdata.json\n");
@@ -8,13 +27,9 @@ fs.readFile('./appdata.json', function (err, data) {
         var json = JSON.parse(data.toString('utf8'));
         var len = json.applications.length;
         for (var i = 0; i < len; i++) {
-            /*FIXME: json add a attribute? how to ? */
-            var app = json.applications[i];
-            var str = JSON.stringify(app);
-            var pos = str.lastIndexOf('}');
-            var new_str = str.substr(0, pos) + ","
-                + "\"repo\":" + "\"" + json["repo"] + "\"}";
-            var new_object = JSON.parse (new_str);
-            $('test.content').insert (new_object);
+            add_content (json.applications [i], json["repo"]);
+            add_category (json.applications [i]);
+            /*TODO: license or lots of other infos should be added */
         }
+    }
 });
