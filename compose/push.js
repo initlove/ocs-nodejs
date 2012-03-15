4178b49f1c91e1be9c01b863c6a6a3aacb00143f
David Liang <liangchenye@gmail.com>
2012-03-15 09:28:32 +0800

    learn how to add attribute to a json obj

--- compose/push.js
+++ compose/push.js
@@ -1,17 +1,6 @@
 var fs = require('fs');
 var $ = require("mongous").Mongous;
 
-add_content = function (app, repo) {
-    /*FIXME: json add a attribute? how to ? */
-    var str = JSON.stringify(app);
-    var pos = str.lastIndexOf('}');
-    var new_str = str.substr(0, pos) + ","
-                + "\"repo\":" + "\"" + repo + "\"}";
-    var new_object = JSON.parse (new_str);
-    /*TODO: check, update */
-    $('test.content').save (new_object);
-};
-
 add_category = function (app) {
     console.log (app.appcategories);
     for (var i = 0; i < app.appcategories.length; i++) {
@@ -27,7 +16,8 @@ fs.readFile('./appdata.json', function (err, data) {
         var json = JSON.parse(data.toString('utf8'));
         var len = json.applications.length;
         for (var i = 0; i < len; i++) {
-            add_content (json.applications [i], json["repo"]);
+            json.applications [i].repo = json.repo;
+            $('test.content').save (json.applications [i]);
             add_category (json.applications [i]);
             /*TODO: license or lots of other infos should be added */
         }
---
+++ test/json.js
@@ -0,0 +1,8 @@
+
+var a = {"name" : "initlove", "email": "liangchenye@gmail.com"};
+
+console.log (a);
+
+a.car = "civic";
+
+console.log (a);
