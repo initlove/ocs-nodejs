30ea55dc3f73dde1a5008dbcb386f6477d04b255
David Liang <liangchenye@gmail.com>
2012-07-09 17:55:35 +0800

    make better s3

--- api/s3.js
+++ api/s3.js
@@ -2,40 +2,32 @@ var walk = require('walk');
 var path = require('path');
 var fs = require('fs');
 var mime = require('mime');
+var utils = require('./utils');
 
 var default_cache = "/data/s3/";
 
-function get_dir(url, res) {
-    var walker  = walk.walk(url, { followLinks: false });
-    var files = [];
-    var val = {'status' : 'ok'};
-
-    walker.on('file', function(root, stat, next) {
-        var item = {};
-        item.name = stat.name;
-        item.type = 'file';
-        files.push(item);
-        next();
-    });
-    walker.on('directory', function(root, stat, next) {
-        var item = {};
-        item.name = stat.name;
-        item.type = 'dir';
-        files.push(item);
-        next();
-    });
-
-    walker.on('end', function() {
-        val.files = files;
-        res.send(val);
+function get_dir(req, res, url) {
+    console.log('get dir'+ url);
+    fs.readdir(url, function(err, files) {
+        var val = {};
+        if (err) {
+            val.status = "fail";
+            val.errno = err.errno;
+        } else {
+            val.status = "ok";
+            val.files = files;
+        }
+        utils.info(req, res, val);
     });
 };
 
-function get_file(url, res) {
+function get_file(req, res, url) {
+    console.log('get file' + url);
+
     fs.readFile(url, function(err, data) {
         if (err) {
             var val = {'status': 'error'};
-            res.send(val);
+            utils.info(req, res, val);
         } else {
             res.writeHead(200, {'Content-Type': mime.lookup(url)});
             res.end(data);
@@ -45,8 +37,12 @@ function get_file(url, res) {
 
 exports.get = function(req, res) {
     var url = req.query.url;
+    var val = {};
+
     if (!url) {
-        res.send("Define the url first");
+        val.status = "fail";
+        val.message = "define the url first";
+        utils.info(req, res, val);
         return;
     }
     var real_url = path.join(default_cache, url);
@@ -56,26 +52,29 @@ exports.get = function(req, res) {
             var val = {};
             val.status = "error";
             val.errno = err.errno;
-            res.send(val);
+            utils.info(req, res, val);
         } else {
             if (stats.isDirectory()) {
-                get_dir(real_url, res);
+                get_dir(req, res, real_url);
             } else if (stats.isFile()) {
-                get_file(real_url, res);
+                get_file(req, res, real_url);
             }
         }
     });
 };
 
 exports.get_info = function(req, res) {
+    console.log('get info ' + req.query.url);
     var url = req.query.url;
+    var val = {};
+
     if (!url) {
-        res.send("Define the url first");
+        val.status = "fail";
+        val.message = "define the url first";
+        utils.info(req, res, val);
         return;
     }
     var real_url = path.join(default_cache, url);
-
-    var val = {};
 
     fs.stat(real_url, function (err, stats) {
         if (err) {
@@ -89,17 +88,24 @@ exports.get_info = function(req, res) {
                 val.type = "file";
             }
         }
-        res.send(val);
+        utils.info(req, res, val);
     });
 };
 
 exports.add_dir = function(req, res) {
-    var url = req.query.url;
+    //trick. curl seems not good with both upload and body info
+    var url = '';
+    console.log(req);
+    if (req.query.url)
+        url = req.query.url;
+    else
+        url = req.body.url;
+    console.log ('add dir ' + url);
     var val = {};
     if (!url) {
         val.status = "fail";
         val.message = "define the url first";
-        res.send(val);
+        utils.info(req, res, val);
         return;
     }
     var real_url = path.join(default_cache, url);
@@ -108,54 +114,50 @@ exports.add_dir = function(req, res) {
             val.status = "fail";
             console.log(err);
             val.errno = err.errno;
-            res.send(val);
+            utils.info(req, res, val);
         } else {
             val.status = "ok";
-            res.send(val);
+            utils.info(req, res, val);
         }
     });
 };
 
 exports.add = function(req, res) {
-    var url = req.query.url;
+    var url = '';
+    if (req.query.url)
+        url = req.query.url;
+    else if (req.body.url)
+        url = req.body.url;
+
+    var val = {};
     if (!url) {
-        res.send("Define the url first");
+        val.status = "error";
+        val.message = "Define url first";
+        utils.info(req, res, val);
         return;
     }
     //TODO: check, if the url is dir, we add file to that dir
     var real_url = path.join(default_cache, url);
-    var val = {};
 
-    req.form.complete(function(err, fields, files){
+    console.log('\nuploaded %s %s'
+                ,  req.files.upload.filename
+                ,  req.files.upload.path);
+    console.log(real_url);
+    fs.stat(real_url, function(err, stat) {
         if (err) {
-            val.status = "error";
-            val.error = err;
-            res.send(val);
-        } else {
-// curl -F upload=@./upload_text localhost:3000/s3/file?url=location
-            console.log('\nuploaded %s %s'
-                ,  files.upload.filename
-                ,  files.upload.path);
-            console.log(real_url);
-            var stat = fs.statSync(real_url);
-            if (stat && stat.isDirectory())
-                real_url = path.join(real_url, files.upload.filename);
-//FIXME: only real_url with exist dir can be used
-// cannot rename in the different disk: nodejs bug
-            fs.rename(files.upload.path, real_url, function (err) {
-                if (err) {
-                    val.status = "error";
-                    val.errno = err.errno;
-                } else {
-                    val.status = "ok";
-                }
-                res.send(val);
-            });
+        } else if (stat.isDirectory()) {
+            real_url = path.join(real_url, files.upload.filename);
         }
-      });
-
-    req.form.on('progress', function(bytesReceived, bytesExpected){
-        var percent = (bytesReceived / bytesExpected * 100) | 0;
-        process.stdout.write('Uploading: %' + percent + '\r');
+        //FIXME: only real_url with exist dir can be used
+        // cannot rename in the different disk: nodejs bug
+        fs.rename(req.files.upload.path, real_url, function (err) {
+            if (err) {
+                val.status = "error";
+                val.errno = err.errno;
+            } else {
+                val.status = "ok";
+            }
+            utils.info(req, res, val);
+        });
     });
 };
